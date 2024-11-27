1c65145db2af738b4e863e8261d5873564fc0d9a
Christina <184446177+ckim53@users.noreply.github.com>
2024-11-26 22:22:06 -0800

    Create playGame function

--- javascript.js
+++ javascript.js
@@ -1,6 +1,3 @@
-let humanScore = 0;
-let computerScore = 0;
-
 function getComputerChoice(){
     //generate a random number between 1 and 3
     const num = Math.floor(Math.random() * 3) + 1;
@@ -22,57 +19,71 @@ function getHumanChoice(){
     return input;
 }
 
-function playRound(humanChoice, computerChoice){
-    if(humanChoice == "rock"){
-        switch (computerChoice){
-            case ("paper"):
-                computerScore += 1;
-                console.log("You lose! Paper beats rock.");
-                break;
-            case("scissors"):
-                humanScore += 1;
-                console.log("You win! Rock beats scissors.");
-                break;
-            default:
-                console.log("It's a tie!");
-                break;
+
+
+function playGame(){
+    let humanScore = 0;
+    let computerScore = 0;
+    
+    function playRound(humanChoice, computerChoice){
+        if(humanChoice == "rock"){
+            switch (computerChoice){
+                case ("paper"):
+                    computerScore += 1;
+                    console.log("You lose! Paper beats rock.");
+                    break;
+                case("scissors"):
+                    humanScore += 1;
+                    console.log("You win! Rock beats scissors.");
+                    break;
+                default:
+                    console.log("It's a tie!");
+                    break;
+            }
+        }
+        else if(humanChoice == "paper"){
+            switch (computerChoice){
+                case ("scissors"):
+                    computerScore += 1;
+                    console.log("You lose! Scissors beats paper.");
+                    break;
+                case("rock"):
+                    humanScore += 1;
+                    console.log("You win! Paper beats rock.");
+                    break;
+                default:
+                    console.log("It's a tie!");
+                    break;
+            }
+        }
+        else {
+            switch (computerChoice){
+                case ("rock"):
+                    computerScore += 1;
+                    console.log("You lose! Rock beats scissors.");
+                    break;
+                case("paper"):
+                    humanScore += 1;
+                    console.log("You win! Scissors beats paper.");
+                    break;
+                default:
+                    console.log("It's a tie!");
+                    break;
+            }
         }
     }
-    else if(humanChoice == "paper"){
-        switch (computerChoice){
-            case ("scissors"):
-                computerScore += 1;
-                console.log("You lose! Scissors beats paper.");
-                break;
-            case("rock"):
-                humanScore += 1;
-                console.log("You win! Paper beats rock.");
-                break;
-            default:
-                console.log("It's a tie!");
-                break;
-        }
+
+    for (let i = 0; i < 5; i++){
+        playRound(getHumanChoice(), getComputerChoice());
     }
-    else {
-        switch (computerChoice){
-            case ("rock"):
-                computerScore += 1;
-                console.log("You lose! Rock beats scissors.");
-                break;
-            case("paper"):
-                humanScore += 1;
-                console.log("You win! Scissors beats paper.");
-                break;
-            default:
-                console.log("It's a tie!");
-                break;
-        }
+    if (humanScore > computerScore){
+        console.log("Congrats! You won the game!");
     }
+    else if (humanScore < computerScore){
+        console.log("The computer won. Better luck next time!");
+    }
+    else{
+        console.log("It's a tie! Good game.");
+    }
+
 }
-
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
-console.log(`Your score: ${humanScore}`)
-console.log(`Computer score: ${computerScore}`)
